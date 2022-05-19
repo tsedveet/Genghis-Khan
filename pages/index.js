a2abafa2c6299d6b8aea93497eda01649b37ba18
tsedveet <tsedvee.tt@gmail.com>
2022-05-19 18:58:53 +0800

    tailwindcss framewrok nemlee

--- pages/index.js
+++ pages/index.js
@@ -3,5 +3,5 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Genghis Khan</div>;
+  return <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>;
 }
